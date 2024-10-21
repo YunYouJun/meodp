eb0c7e560168970d01d3a0e0ce3f45d5674406c2
YunYouJun <me@yunyoujun.cn>
2024-10-21 23:49:53 +0800

    feat: check site links

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,10 @@
 import type { Page } from 'playwright'
-import { consoleInnerInfo, errorStart, successStart } from 'cilicili'
+import { url } from 'node:inspector'
+import process from 'node:process'
+import { cursorTo, moveCursor } from 'node:readline'
+import { consoleInnerInfo, errorStart, lineStart, successStart } from 'cilicili'
 import consola from 'consola'
+
 import { colors } from 'consola/utils'
 
 /**
@@ -8,43 +12,92 @@ import { colors } from 'consola/utils'
  * - css
  * - js
  * - images
+ * 检测所有匹配资源，包括没有被加载的 legacy 资源
  */
 export async function checkPageAssets(page: Page) {
-  const cssAndJsLinks = await page.$$eval('link[rel=stylesheet], script', elements =>
-    elements.map(element => element.getAttribute('src') || element.getAttribute('href')))
+  // const cssAndJsLinks = await page.$$eval('link[rel=stylesheet], script', (elements) => {
+  //   return elements
+  //     .map(element => element.getAttribute('src') || element.getAttribute('href'))
+  //     .filter(Boolean)
+  // })
 
-  const pageUrl = page.url()
-  consola.start('Checking assets in page:', colors.cyan(pageUrl))
+  // const pageUrl = page.url()
+  // consola.start('Checking assets in page:', colors.cyan(pageUrl))
+  // // 并行
+  // const loadPromisesArr: any[] = []
+  // cssAndJsLinks.forEach((link) => {
+  //   loadPromisesArr.push(
+  //     (async () => {
+  //       console.log('link:', link)
+  //       // response
+  //       if (!link)
+  //         return
 
-  for (let link of cssAndJsLinks) {
-    // response
-    if (!link)
-      continue
+  //       if (link.startsWith('//')) {
+  //         link = `https:${link}`
+  //       }
+  //       else if (link.startsWith('/')) {
+  //         link = new URL(link, pageUrl).href
+  //       }
+  //       const startTime = Date.now()
+  //       const response = await page.goto(link)
+  //       const statusCode = response?.status() || 'unknown'
+  //       const linkTxt = colors.dim(link)
+  //       const statusText = response?.statusText()
+  //       const statusInfo = statusText ? `${statusCode?.toString()} ${statusText}` : statusCode?.toString()
 
-    if (link.startsWith('//')) {
-      link = `https:${link}`
-    }
-    else if (link.startsWith('/')) {
-      link = new URL(link, pageUrl).href
-    }
-    const startTime = Date.now()
-    const response = await page.goto(link)
+  //       const duration = Date.now() - startTime
+  //       // const durationTxt = colors.dim(colors.magenta(`[${duration}ms]`))
+  //       // paddingSpace
+  //       const durationTxt = colors.dim(`│${colors.blue(`${duration.toString().padStart(4, ' ')}${colors.white('ms')}`)} │`)
+
+  //       if (statusCode !== 200) {
+  //         consoleInnerInfo(colors.dim(errorStart), colors.dim(colors.red(statusInfo)), durationTxt, linkTxt)
+  //       }
+  //       else {
+  //         consoleInnerInfo(colors.dim(successStart), colors.dim(colors.green(statusInfo)), durationTxt, linkTxt)
+  //       }
+  //       return 'done'
+  //     })(),
+  //   )
+  // })
+  // console.log('loadPromises:', loadPromisesArr)
+  // await Promise.all(loadPromisesArr)
+
+  consola.success(colors.green('All assets checked.'))
+
+  await page.goto
+}
+
+export function registerPageEvents(page: Page) {
+  const urlMap = new Map<string, number>()
+
+  // 监听所有的网络请求
+  page.on('request', (request) => {
+    urlMap.set(request.url(), Date.now())
+  })
+
+  // 监听所有的网络响应
+  page.on('response', (response) => {
+    // console.log('<<', response.status(), response.url())
+    const startTime = urlMap.get(response.url()) || Date.now()
+    const duration = Date.now() - startTime
+
     const statusCode = response?.status() || 'unknown'
-    const linkTxt = colors.dim(link)
+    const linkTxt = colors.dim(response.url())
     const statusText = response?.statusText()
     const statusInfo = statusText ? `${statusCode?.toString()} ${statusText}` : statusCode?.toString()
-
-    const duration = Date.now() - startTime
-    // const durationTxt = colors.dim(colors.magenta(`[${duration}ms]`))
-    // paddingSpace
     const durationTxt = colors.dim(`│${colors.blue(`${duration.toString().padStart(4, ' ')}${colors.white('ms')}`)} │`)
 
-    if (statusCode !== 200) {
-      consoleInnerInfo(colors.dim(errorStart), colors.dim(colors.red(statusInfo)), durationTxt, linkTxt)
+    if (response.status() >= 400) {
+      console.error(`Resource loading error: ${response.status()} ${response.statusText()} ${response.url()}`)
     }
     else {
-      consoleInnerInfo(colors.dim(successStart), colors.dim(colors.green(statusInfo)), durationTxt, linkTxt)
+      // consoleInnerInfo(colors.dim(successStart), colors.dim(colors.green(statusInfo)), durationTxt, linkTxt)
+      process.stdout.write(`${lineStart} ${colors.dim(successStart)} ${colors.dim(colors.green(statusInfo))} ${durationTxt} ${linkTxt}`)
+      process.stdout.clearLine(0)
+      // 光标移动到开头，覆盖输出
+      cursorTo(process.stdout, 0)
     }
-  }
-  consola.success(colors.green('All assets checked.'))
+  })
 }
--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,5 +1,81 @@
+import type { Page } from 'playwright'
+import consola from 'consola'
+import { colors } from 'consola/utils'
 import { getBrowser } from '../env'
-import { checkPageAssets } from './assets'
+import { checkPageAssets, registerPageEvents } from './assets'
+
+/**
+ * 只检查内部链接
+ */
+// const onlyCheckInternalLink = true
+const siteUrls = new Set<string>()
+
+/**
+ * 获取站点外链
+ */
+export async function getSiteLinks(page: Page) {
+  const links = await page.$$eval('a', (elements) => {
+    return elements
+      .map(element => element.getAttribute('href'))
+      .filter(Boolean)
+  })
+
+  /**
+   * 过滤 query
+   */
+  const filterQuery = true
+  const onlyCheckInternalLink = true
+
+  const filterLinks = links.map((link) => {
+    if (!link) {
+      return false
+    }
+
+    if (onlyCheckInternalLink) {
+      if (link.startsWith('/')) {
+        if (filterQuery) {
+          // remove query (env: node) searchParams
+          const url = new URL(link, page.url())
+          url.search = ''
+          return url.toString()
+        }
+        return link
+      }
+    }
+    return false
+  }).filter(Boolean)
+
+  return filterLinks as string[]
+}
+
+/**
+ * check site url
+ * @param url
+ */
+export async function checkSiteUrl(url: string) {
+  const browser = await getBrowser()
+  const page = await browser.newPage()
+
+  registerPageEvents(page)
+  consola.start('Checking site internal url', colors.cyan(url))
+  await page.goto(url, {
+    waitUntil: 'networkidle',
+  }).catch((e) => {
+    consola.error(e)
+  })
+  consola.success('All request loaded.')
+  console.log()
+
+  const links = await getSiteLinks(page)
+  for (const link of links) {
+    if (!siteUrls.has(link)) {
+      siteUrls.add(link)
+      const url = new URL(link, page.url()).href
+      await checkSiteUrl(url)
+    }
+  }
+  await page.close()
+}
 
 /**
  * 检查站点可访问性
@@ -9,7 +85,38 @@ import { checkPageAssets } from './assets'
 export async function checkSite(url: string) {
   const browser = await getBrowser()
   const page = await browser.newPage()
-  await page.goto(url)
 
-  await checkPageAssets(page)
+  if (!url.endsWith('/')) {
+    url += '/'
+  }
+  siteUrls.add(url)
+
+  registerPageEvents(page)
+  consola.start('Checking site home url:', colors.cyan(url))
+  const startTime = Date.now()
+  await page.goto(url, {
+    waitUntil: 'networkidle',
+  })
+  consola.success('All request loaded.')
+  console.log()
+
+  const links = await getSiteLinks(page)
+  await page.close()
+
+  for (const link of links) {
+    if (!siteUrls.has(link)) {
+      siteUrls.add(link)
+      const url = new URL(link, page.url()).href
+      await checkSiteUrl(url)
+    }
+  }
+
+  const endTime = Date.now()
+  consola.info('Site checked in', colors.cyan(`${(endTime - startTime) / 1000}s`))
+
+  await browser.close()
+  consola.success('All site checked.')
+
+  // console siteUrls
+  consola.info('siteUrls:', siteUrls)
 }
